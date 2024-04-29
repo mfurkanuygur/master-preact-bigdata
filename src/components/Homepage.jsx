307cbce17d1d4cd392ed6e0e7843de7bf502d770
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2024-04-29 13:13:06 +0300

    update homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getAllProducts } from "../request/request"
+import Loading from "./Loading"
 
 const Homepage = () => {
     const [products, setProducts] = useState()
@@ -20,16 +21,17 @@ const Homepage = () => {
         <div className="homepage">
             <h1>All Products: {products?.length}</h1>
             <div className="container">
-                {
+                {products &&
                     products?.slice(0, count).map(product => (
                         <div key={product.id} className="pokemon-card">
                             <div className="pokemon-img"><img src={product?.image} alt={product?.title} /></div>
                             <h3>{product.title}</h3>
                         </div>
                     ))
+                    || <Loading />
                 }
-                <button onClick={() => handleClick()}>see more</button>
             </div>
+            <button onClick={() => handleClick()}>see more</button>
         </div>
     )
 }
